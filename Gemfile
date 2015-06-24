c3f122e7ccf1540b1d36ac54fed5250161888e8f
Alex Martin <martin.alex.t+github@gmail.com>
2015-06-24 02:32:54 -0400

    Add 12factor for logging.

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'griddler-mandrill'
 # Usee Whenever for cron job managment from Rails
 gem 'whenever', :require => false
 
+gem 'rails_12factor'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -8,12 +8,12 @@ class EmailProcessor
   def process
     puts "******TRYING TO GET #{@email.to[0].token}!******"
     # return if !Inbox.select(:slug).include? @email.to[0].token
-    EmailMessage.create!({
+    EmailMessage.create({
       to: @email.to[0].email,
       from: @email.from[0].email,
       subject: @email.subject,
       body: @email.body,
-      inbox: Inbox.find_by(slug: @email.to[0].token)
+      #inbox: Inbox.find_by(slug: @email.to[0].token)
     })
   end
 
