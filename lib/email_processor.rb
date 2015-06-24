09c903c648073dcd522935f62551341156cd4219
Alex Martin <martin.alex.t+github@gmail.com>
2015-06-24 02:41:57 -0400

    Fixing syntax on Griddle calls.

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -6,11 +6,11 @@ class EmailProcessor
   end
 
   def process
-    puts "******TRYING TO GET #{@email.to[0].token}!******"
+    puts "******TRYING TO GET #{@email.to[0][:token]}!******"
     # return if !Inbox.select(:slug).include? @email.to[0].token
     EmailMessage.create({
-      to: @email.to[0].email,
-      from: @email.from[0].email,
+      to: @email.to[0][:email],
+      from: @email.from[0][:email],
       subject: @email.subject,
       body: @email.body,
       #inbox: Inbox.find_by(slug: @email.to[0].token)
