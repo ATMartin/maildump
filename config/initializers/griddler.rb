6d9e0e5121cab5c98b3dfa4853e143162a1823f1
Alex Martin <martin.alex.t+github@gmail.com>
2015-06-24 02:23:09 -0400

    Email configuration.

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -1,3 +1,8 @@
+require 'email_processor'
+
 Griddler.configure do |config|
+  config.processor_class = EmailProcessor
+  config.processor_method = :process
+  config.reply_delimiter = '--REPLY ABOVE THIS LINE--'
   config.email_service = :mandrill
 end
--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,12 +1,13 @@
 class EmailProcessor
 
   def initialize (email)
+    puts "**********INIT THE EMAIL**********"
     @email = email
   end
 
   def process
     puts "******TRYING TO GET #{@email.to[0].token}!******"
-    return if !Inbox.select(:slug).include? @email.to[0].token
+    # return if !Inbox.select(:slug).include? @email.to[0].token
     EmailMessage.create!({
       to: @email.to[0].email,
       from: @email.from[0].email,
