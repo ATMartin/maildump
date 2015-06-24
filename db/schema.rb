163fca856da00cf6b6a6c66eec3e56d2c2faa779
Alex Martin <martin.alex.t+github@gmail.com>
2015-06-24 02:01:56 -0400

    Switch to Postgres db - whoops\!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624033322) do
+ActiveRecord::Schema.define(version: 20150624054502) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "email_messages", force: :cascade do |t|
+    t.string   "from"
+    t.string   "to"
+    t.string   "subject"
+    t.string   "body"
+    t.integer  "inbox_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "inboxes", force: :cascade do |t|
     t.string   "slug"
