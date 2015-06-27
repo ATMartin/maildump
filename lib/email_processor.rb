530505028f52e452fe6bd380124d99dac6848b61
Alex Martin <martin.alex.t+github@gmail.com>
2015-06-27 11:41:55 -0400

    Add simple cleanup rake task.

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -7,13 +7,16 @@ class EmailProcessor
 
   def process
     puts "******TRYING TO GET #{@email.to[0][:token]}!******"
-    # return if !Inbox.select(:slug).include? @email.to[0].token
+    if !Inbox.select(:slug).include? @email.to[0][:token]
+      puts "******NO INBOX FOUND FOR #{@email.to[0][:token]}!*****"
+      return
+    end
     EmailMessage.create({
       to: @email.to[0][:email],
       from: @email.from[:email],
       subject: @email.subject,
       body: @email.body,
-      #inbox: Inbox.find_by(slug: @email.to[0].token)
+      inbox: Inbox.find_by(slug: @email.to[0].token)
     })
   end
 
