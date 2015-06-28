344a4d6dce2cd78538ea2a6fa9194afe93649182
Alex Martin <martin.alex.t+github@gmail.com>
2015-06-28 15:10:18 -0400

    Add initializer for Maildump constants.

---
+++ config/initializers/maildump.rb
@@ -0,0 +1,11 @@
+# Constants used by Maildump for various actions.
+
+# Your email subdomain used as a dump address.
+MD_EMAIL_SUBDOMAIN = "trash.atmartin.io"
+
+# The default time for your mailboxes to exist before self-destructing
+MD_DEFAULT_DURATION = 2.minutes
+
+
+
+
