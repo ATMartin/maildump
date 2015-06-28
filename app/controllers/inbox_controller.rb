806b186de09ceb2ae878ee4ca542012efe4ced50
Alex Martin <martin.alex.t+github@gmail.com>
2015-06-28 15:56:29 -0400

    Fix Inbox controller to return proper email list.

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -4,7 +4,7 @@ class InboxController < ApplicationController
     current_inbox = cookies[:maildump_inbox]
     if current_inbox.present? && Inbox.exists?(slug: current_inbox)
       @inbox = Inbox.find_by(slug: current_inbox)
-      @emails = EmailMessage.find_by(inbox_id: @inbox.id).to_a
+      @emails = EmailMessage.where(inbox_id: @inbox.id).to_a
     else 
       new_slug = Inbox.slug_gen
       expiry = Time.now + MD_DEFAULT_DURATION
