f9825f4255044bf21aa010e6fa28a18c0c06e357
Alex Martin <martin.alex.t+github@gmail.com>
2015-06-28 15:11:00 -0400

    Enforce new constants & add cookie management to prevent new-inbox-on-refresh.

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -1,7 +1,16 @@
 class InboxController < ApplicationController
 
   def index
-    @inbox = Inbox.create!(slug: Inbox.slug_gen)
+    current_inbox = cookies[:maildump_inbox]
+    if current_inbox.present? && Inbox.exists?(slug: current_inbox)
+      @inbox = Inbox.find_by(slug: current_inbox)
+    else 
+      new_slug = Inbox.slug_gen
+      expiry = Time.now + MD_DEFAULT_DURATION
+      cookies[:maildump_inbox] = { value: new_slug, expires: expiry }
+      cookies[:maildump_dies_at] = { value: expiry, expires: expiry }
+      @inbox = Inbox.create!(slug: new_slug)
+    end
   end
 
 end
--- lib/tasks/data.rake
+++ lib/tasks/data.rake
@@ -3,7 +3,7 @@ namespace :data do
   
   desc "It deletes old inboxes and their messages."
   task :clean_expired_records => :environment do
-    Inbox.where("created_at < ?", Time.now - 2.minutes).each do |inbox|
+    Inbox.where("created_at < ?", Time.now - MD_DEFAULT_DURATION).each do |inbox|
       puts "Inbox #{inbox.id} is bad!"
       EmailMessage.where(:inbox_id => inbox.id).delete_all
       inbox.delete
