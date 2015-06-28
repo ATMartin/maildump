b82cc0b7bb44988e93241c9e43fe048f9c1f8422
Alex Martin <martin.alex.t+github@gmail.com>
2015-06-28 15:24:55 -0400

    Add simple email display.

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -4,13 +4,16 @@ class InboxController < ApplicationController
     current_inbox = cookies[:maildump_inbox]
     if current_inbox.present? && Inbox.exists?(slug: current_inbox)
       @inbox = Inbox.find_by(slug: current_inbox)
+      @emails = EmailMessage.find_by(inbox_id: @inbox.id).to_a
     else 
       new_slug = Inbox.slug_gen
       expiry = Time.now + MD_DEFAULT_DURATION
       cookies[:maildump_inbox] = { value: new_slug, expires: expiry }
       cookies[:maildump_dies_at] = { value: expiry, expires: expiry }
       @inbox = Inbox.create!(slug: new_slug)
+      @emails = []
     end
+  
   end
 
 end
